0fbd9bff811ef11a90a0bd04fee1c5f6db3f5ec5
Deependra Kumar <deependrarajpoot178@gmail.com>
2022-11-11 22:52:41 +0530

    frontend added

---
+++ frontends/src/App.js
@@ -0,0 +1,66 @@
+import "./App.css";
+import React from "react";
+import googleOneTap from "google-one-tap";
+import { useEffect, useState } from "react";
+
+const options = {
+  client_id:"114914030611-askhm7c3r7pnhp8b7rq87qbvt8ncb2sb.apps.googleusercontent.com", // required
+  auto_select: false, // optional
+  cancel_on_tap_outside: false, // optional
+  context: "signin", 
+};
+
+function App() {
+  const [loginData, setLoginData] = useState(
+    localStorage.getItem("loginData")
+      ? JSON.parse(localStorage.getItem("loginData"))
+      : null
+  );
+  useEffect(() => {
+    if (!loginData) {
+      googleOneTap(options, async (response) => {
+        console.log(response);
+        const res = await fetch("/api/google-login", {
+          method: "POST",
+          body: JSON.stringify({
+            token: response.credential,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const data = await res.json();
+        setLoginData(data);
+        localStorage.setItem("loginData", JSON.stringify(data));
+      });
+    }
+  }, [loginData]);
+
+  const handleLogout = () => {
+    localStorage.removeItem("loginData");
+    setLoginData(null);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Implement "Google One-Tap Login" In React and Node.js</h1>
+        <div>
+          {loginData ? (
+            <div>
+              <h3>
+                You {loginData.name} logged in as {loginData.email}
+              </h3>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          ) : (
+            <div>Not logged in</div>
+          )}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
