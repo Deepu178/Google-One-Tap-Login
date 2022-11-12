0e2b6c451e382f5fb081237451639e1a25433343
Deependra Kumar <deependrarajpoot178@gmail.com>
2022-11-12 17:53:17 +0530

    Added comments for better understanding of code and how it works

--- frontends/src/App.js
+++ frontends/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import React from "react";
+//importing the googleOneTap function
 import googleOneTap from "google-one-tap";
 import { useEffect, useState } from "react";
 
+//creating the options object
 const options = {
+  //client id of the google app
   client_id:"114914030611-askhm7c3r7pnhp8b7rq87qbvt8ncb2sb.apps.googleusercontent.com", // required
   auto_select: false, // optional
   cancel_on_tap_outside: false, // optional
@@ -11,15 +14,18 @@ const options = {
 };
 
 function App() {
+  //creating useState hooks
   const [loginData, setLoginData] = useState(
     localStorage.getItem("loginData")
       ? JSON.parse(localStorage.getItem("loginData"))
       : null
   );
+  //creating the useEffect hook
   useEffect(() => {
     if (!loginData) {
       googleOneTap(options, async (response) => {
         console.log(response);
+        //Ajax call to backend
         const res = await fetch("/api/google-login", {
           method: "POST",
           body: JSON.stringify({
@@ -29,7 +35,7 @@ function App() {
             "Content-Type": "application/json",
           },
         });
-
+        //getting the response from backend
         const data = await res.json();
         setLoginData(data);
         localStorage.setItem("loginData", JSON.stringify(data));
@@ -37,6 +43,7 @@ function App() {
     }
   }, [loginData]);
 
+  //creating the logout function
   const handleLogout = () => {
     localStorage.removeItem("loginData");
     setLoginData(null);
@@ -47,11 +54,13 @@ function App() {
       <header className="App-header">
         <h1>Login with Google</h1>
         <div>
+          {/* checking if the user is logged in or not */}
           {loginData ? (
             <div>
               <h3>
                 You logged in as {loginData.name} 
               </h3>
+              {/* calling the logout function */}
               <button onClick={handleLogout}>Logout</button>
             </div>
           ) : (
